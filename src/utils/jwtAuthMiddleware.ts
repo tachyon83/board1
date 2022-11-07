3d3df479992f9fd73c3847d672e557aaec977971
tachyon83 <83tachyon@gmail.com>
2022-11-08 02:27:30 +0900

    [fix] jwt 인증 미들웨어 함수 수정

--- src/utils/jwtAuthMiddleware.ts
+++ src/utils/jwtAuthMiddleware.ts
@@ -1,49 +1,39 @@
-import { verify, updateToken, IJwtVerificaitonResponse } from './jwtUtils'
+import { verify, updateToken, IJwtVerificationResponse } from './jwtUtils'
 import * as express from 'express'
+import {ErrorString} from "./enums";
+import {CustomError} from "../middlewares/error.handler";
 
 export const jwtAuth = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
-  // const accessToken = req.headers[process.env.JWT_ACCESS_TOKEN_KEY]
-  const accessToken = req.headers.JWT_ACCESS_TOKEN_KEY
-  const refreshToken = req.headers[process.env.JWT_REFRESH_TOKEN_KEY]
+  const accessToken = req.headers.jwt_access_token_key
+  // const refreshToken = req.headers[process.env.JWT_REFRESH_TOKEN_KEY]
 
-  if (!accessToken || !refreshToken) {
-    throw new Error('no authentication token')
-    // return res.json({
-    //   result: false,
-    //   code: resCode.notAuthenticated,
-    //   data: null,
-    // })
+  if (!accessToken) {
+    return next(new CustomError(ErrorString.WrongJWT, 'jwtAuth_noAccessToken'))
   }
 
   try {
-    const jwtVerification: IJwtVerificaitonResponse = await verify(accessToken)
+    const jwtVerification: IJwtVerificationResponse = await verify(accessToken)
     req.contextUserId = jwtVerification.decodedUserId
     return next()
   } catch (jwtVerification) {
     switch (jwtVerification.jwtVerificationError) {
-      // 토큰 만료
-      case 'TokenExpiredError':
-        const newAccessToken = await updateToken(refreshToken)
-        if (newAccessToken) {
-          req.newAccessToken = newAccessToken
-          return next()
-        }
-        return res.json({ ok: 'login session expired...need to re-login' })
+      // // 토큰 만료
+      // case 'TokenExpiredError':
+      //   const newAccessToken = await updateToken(refreshToken)
+      //   if (newAccessToken) {
+      //     req.newAccessToken = newAccessToken
+      //     return next()
+      //   }
+      //   return res.json({ ok: 'login session expired...need to re-login' })
 
       // 토큰 오염
-      case 'JsonWebTokenError':
-        return res.json({ result: 'invalid token' })
+      case 'JsonWebTokenError': return next(new CustomError(ErrorString.WrongJWT, 'jwtAuth_pollutedJwt'))
 
-      default:
-        console.log(
-          'unknown...but error anyways',
-          jwtVerification.jwtVerificationError
-        )
-        return res.json({ result: 'unknown error case' })
+      default: return next(new CustomError(ErrorString.WrongJWT, 'jwtAuth_unknownErrorInJwt'))
     }
   }
 }
