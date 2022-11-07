815e2a7fea771261e6cfddae360db50d8d47653c
tachyon83 <83tachyon@gmail.com>
2022-11-08 02:28:08 +0900

    [fix] 에러 핸들러 수정 (status, 로그 기능)

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -1,9 +1,32 @@
-export const errorHandler = (err, req, res, next) => {
-  const status = err.status
-  const message = err.message
+import {ErrorString} from "../utils/enums"
+import {AppDataSource} from "../data-source"
+import {ErrorLog} from "../modules/ErrorLog/ErrorLog"
+
+export class CustomError extends Error {
+  path
+  constructor(message,path) {
+    super(message)
+    this.path=path
+  }
+}
+
+export const errorHandler = async (err, req, res, next) => {
+  const status = (e=>{
+    // TODO: turn on/off logging upon serverOption
+    switch (e.message) {
+      case ErrorString.BadClientRequest: return 400
+      case ErrorString.WrongJWT: return 401
+      case ErrorString.DuplicateUserName: return 409
+      default: return 500
+    }
+  })(err)
+
+  const repo=AppDataSource.getRepository(ErrorLog)
+  const inst=await repo.create({errorString: err.message, path: err.path})
+  await repo.insert(inst)
 
   res.status(status).json({
     status,
-    message,
+    message:err.message,
   })
 }
