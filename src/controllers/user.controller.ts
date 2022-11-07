cba57114129c62c72707921f32e2a465fa58a21c
tachyon83 <83tachyon@gmail.com>
2022-11-08 02:29:46 +0900

    [fix] 유저 컨트롤러 수정

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -17,7 +17,7 @@ export default class UserController extends BaseController {
         const router = Router()
 
         router.post('/', requestHandler(this.create))
-        router.post('/login', jwtAuth, requestHandler(this.login))
+        router.post('/login', requestHandler(this.login))
         router.patch('/', jwtAuth, requestHandler(this.update))
         router.post('/delete', jwtAuth, requestHandler(this.delete))
 
@@ -33,7 +33,7 @@ export default class UserController extends BaseController {
     }
 
     update = async (req: express.Request, res: express.Response) => {
-        return this.service.update(req.body.username, req.contextUserId)
+        return this.service.update(req.body, req.contextUserId)
     }
 
     delete = async (req: express.Request, res: express.Response) => {
